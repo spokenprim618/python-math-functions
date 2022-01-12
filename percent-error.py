e89872bce01d89411f1694d4fc488dfc34196898
william vasquez <85375345+spokenprim618@users.noreply.github.com>
2022-01-11 19:31:06 -0500

    Update percent-error.py

--- percent-error.py
+++ percent-error.py
@@ -1,11 +1,4 @@
-# def percentError(actual, experimental):
-#     first= experimental - actual
-#     second= abs(first)
-#     third= second/ actual
-#     fourth= third *100
-#     fifth = round(fourth,1)
-#     return fifth
-# print(percentError(10.9,11.6))
+
 def percentError(actual, experimental):
     return round(((abs(experimental - actual))/actual)*100, 1)
 print(percentError(10.9,11.6))
