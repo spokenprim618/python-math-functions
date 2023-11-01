3ce670eff35a1dd52dee39f52efa8aec71391de9
william vasquez <85375345+spokenprim618@users.noreply.github.com>
2023-11-01 13:49:57 -0400

    Update law-of-cosines.py

--- law-of-cosines.py
+++ law-of-cosines.py
@@ -1,19 +1,44 @@
 import math
 import numpy as np
+# for some reason b is c and c is b
+def sides(a=0,b=0,c=0, 
+          A=0,B=0,C=0):
+   
+    sideA = (b**2+c**2)+((-2*b*c)*
+    (np.cos((A * np.pi)/180)));
+    
+    sideB = (a**2+c**2)+((-2*a*c)*
+    (np.cos((B * np.pi)/180)));
+    
+    sideC = (b**2+a**2)+((-2*b*a)*
+    (np.cos((C * np.pi)/180)));
+    
+    return [
+    round(math.sqrt(sideA),1),
+    round(math.sqrt(sideB),1),
+    round(math.sqrt(sideC),1),
+    ]
 
-def angle(a,b,c):
+print(sides(A=36,B=0,C=0, 
+            a=0,b=5,c=7));
+
+def angle(
+    a1=sides(A=36,b=7,c=5)[0],
+    b1=sides(A=36,b=7,c=5)[1],
+    c1=sides(A=36,b=7,c=5)[2]
+    ):
+        
+    part1A = b1**2 + c1**2;
+    part1B = a1**2 + c1**2;
+    part1C = b1**2 + a1**2;
     
-    part1A = b**2 + c**2;
-    part1B = a**2 + c**2;
-    part1C = b**2 + a**2;
+    part2C = -2*(b1 * a1);
+    part2A = -2*(b1 * c1);
+    part2B = -2*(c1 * a1);
     
-    part2C = -2*(b * a);
-    part2A = -2*(b * c);
-    part2B = -2*(c * a);
-    
-    part3A = a**2 - part1A;
-    part3B = b**2 - part1B;
-    part3C = c**2 - part1C;
+    part3A = a1**2 - part1A;
+    part3B = b1**2 - part1B;
+    part3C = c1**2 - part1C;
     
     part4A = part3A / part2A;
     part4B = part3B / part2B;
@@ -22,25 +47,10 @@ def angle(a,b,c):
     angleA = math.acos(part4A);
     angleB = math.acos(part4B);
     angleC = math.acos(part4C);
-
+    
     return [
-    round(math.degrees(angleA),1),
-    round(math.degrees(angleB),1),
-    round(math.degrees(angleC),1)]
-
-print(angle(4.3,5,6))
-
-def sides(a=0,b=0,c=0, A=0,B=0,C=0):
-    sideA = (b**2+c**2)+((-2*b*c)*
-    (np.cos((46 * np.pi)/180)))
-    
-    sideB = (a**2+c**2)+((-2*a*c)*
-    (np.cos((46 * np.pi)/180)))
-    
-    sideC = (b**2+a**2)+((-2*b*a)*
-    (np.cos((46 * np.pi)/180)))
-    
-    return [math.sqrt(sideA),math.sqrt(sideB),math.sqrt(sideC)]
-
-print(sides(A=46,b=5,c=6))
-    
+    round(math.degrees(angleA),0),
+    round(math.degrees(angleB),0),
+    round(math.degrees(angleC),0)
+    ]
+print(angle())
